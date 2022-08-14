3f4b21af4d19d89119a85cd5de08b97af82f5ff2
Sad Ellie <openthiswhitedoor@gmail.com>
2022-08-14 11:39:08 +0300

    Fixed chips row width

--- app/src/main/java/com/sadellie/unitto/screens/second/components/ChipsRow.kt
+++ app/src/main/java/com/sadellie/unitto/screens/second/components/ChipsRow.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
@@ -67,7 +68,9 @@ fun ChipsRow(
     lazyListState: LazyListState
 ) {
     LazyRow(
-        modifier = Modifier.background(MaterialTheme.colorScheme.background),
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxWidth(),
         state = lazyListState,
         contentPadding = PaddingValues(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
