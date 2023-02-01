2c1278e5a240784fb11dac2bf196f43a7b66c173
Sad Ellie <sadellie.dev@gmail.com>
2023-02-01 19:13:06 +0400

    Added "Notice" for Tools screen

--- feature/tools/src/main/java/com/sadellie/unitto/feature/tools/ToolsScreen.kt
+++ feature/tools/src/main/java/com/sadellie/unitto/feature/tools/ToolsScreen.kt
@@ -1,16 +1,23 @@
 package com.sadellie.unitto.feature.tools
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sadellie.unitto.core.ui.common.UnittoLargeTopAppBar
 
 @Composable
@@ -24,11 +31,38 @@ internal fun ToolsScreen(
     ) { padding ->
         LazyColumn(contentPadding = padding) {
             item {
+                Card(
+                    Modifier
+                        .clickable(onClick = {})
+                        .padding(16.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.tools_notice_title),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Text(
+                            text = stringResource(R.string.tools_notice_description),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Text(
+                            modifier = Modifier.align(Alignment.End),
+                            text = stringResource(R.string.click_to_read_more),
+                            style = MaterialTheme.typography.labelLarge
+                        )
+                    }
+                }
+            }
+            item {
                 ListItem(
                     leadingContent = {
                         Icon(
                             Icons.Default.Schedule,
-                            null // TODO describe
+                            stringResource(R.string.epoch_converter)
                         )
                     },
                     headlineText = { Text(stringResource(R.string.epoch_converter)) },
