4668a5cea36cc3e618ecac83fa9610962fb0b0b7
Sad Ellie <sadellie.dev@gmail.com>
2023-02-21 15:01:25 +0400

    Backspace icon, finally

--- core/ui/src/main/java/com/sadellie/unitto/core/ui/common/key/unittoicons/Delete.kt
+++ core/ui/src/main/java/com/sadellie/unitto/core/ui/common/key/unittoicons/Delete.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
 import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.group
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.sadellie.unitto.core.ui.common.key.UnittoIcons
@@ -17,30 +18,51 @@ val @receiver:Suppress("UNUSED") UnittoIcons.Delete: ImageVector
             return _delete!!
         }
         _delete = Builder(name = "Delete", defaultWidth = 150.0.dp, defaultHeight = 150.0.dp,
-                viewportWidth = 150.0f, viewportHeight = 150.0f).apply {
-            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+            viewportWidth = 150.0f, viewportHeight = 150.0f).apply {
+            group {
+                path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
                     strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
                     pathFillType = NonZero) {
-                moveTo(66.675f, 79.944f)
-                curveTo(65.822f, 80.371f, 64.904f, 80.755f, 63.923f, 81.096f)
-                curveTo(62.984f, 81.437f, 61.96f, 81.715f, 60.851f, 81.928f)
-                curveTo(61.96f, 82.184f, 62.984f, 82.483f, 63.923f, 82.824f)
-                curveTo(64.904f, 83.123f, 65.822f, 83.507f, 66.675f, 83.976f)
-                lineTo(95.411f, 98.504f)
-                curveTo(96.264f, 98.931f, 96.862f, 99.421f, 97.203f, 99.976f)
-                curveTo(97.544f, 100.531f, 97.715f, 101.149f, 97.715f, 101.832f)
-                verticalLineTo(110.024f)
-                lineTo(48.243f, 84.296f)
-                verticalLineTo(79.56f)
-                lineTo(97.715f, 53.896f)
-                verticalLineTo(62.024f)
-                curveTo(97.715f, 62.749f, 97.544f, 63.389f, 97.203f, 63.944f)
-                curveTo(96.862f, 64.456f, 96.264f, 64.925f, 95.411f, 65.352f)
-                lineTo(66.675f, 79.944f)
-                close()
+                    moveTo(70.0f, 99.063f)
+                    lineTo(87.5f, 81.563f)
+                    lineTo(105.0f, 99.063f)
+                    lineTo(111.719f, 92.344f)
+                    lineTo(94.063f, 75.0f)
+                    lineTo(111.406f, 57.656f)
+                    lineTo(104.688f, 50.938f)
+                    lineTo(87.5f, 68.438f)
+                    lineTo(70.0f, 50.938f)
+                    lineTo(63.281f, 57.656f)
+                    lineTo(80.938f, 75.0f)
+                    lineTo(63.281f, 92.344f)
+                    lineTo(70.0f, 99.063f)
+                    close()
+                    moveTo(18.75f, 75.0f)
+                    lineTo(45.156f, 37.656f)
+                    curveTo(46.51f, 35.781f, 48.125f, 34.245f, 50.0f, 33.047f)
+                    curveTo(51.875f, 31.849f, 53.958f, 31.25f, 56.25f, 31.25f)
+                    horizontalLineTo(121.875f)
+                    curveTo(124.453f, 31.25f, 126.66f, 32.168f, 128.496f, 34.004f)
+                    curveTo(130.332f, 35.84f, 131.25f, 38.047f, 131.25f, 40.625f)
+                    verticalLineTo(109.375f)
+                    curveTo(131.25f, 111.953f, 130.332f, 114.16f, 128.496f, 115.996f)
+                    curveTo(126.66f, 117.832f, 124.453f, 118.75f, 121.875f, 118.75f)
+                    horizontalLineTo(56.25f)
+                    curveTo(53.958f, 118.75f, 51.875f, 118.151f, 50.0f, 116.953f)
+                    curveTo(48.125f, 115.755f, 46.51f, 114.219f, 45.156f, 112.344f)
+                    lineTo(18.75f, 75.0f)
+                    close()
+                    moveTo(30.469f, 75.0f)
+                    lineTo(54.531f, 109.375f)
+                    horizontalLineTo(121.875f)
+                    verticalLineTo(40.625f)
+                    horizontalLineTo(54.531f)
+                    lineTo(30.469f, 75.0f)
+                    close()
+                }
             }
         }
-        .build()
+            .build()
         return _delete!!
     }
 
