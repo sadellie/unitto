8150b556ed01302f170e24e2d16fd7d3e888d4fd
Sad Ellie <sadellie.dev@gmail.com>
2023-09-30 22:44:35 +0300

    Remove cache indicator

--- feature/settings/src/main/java/com/sadellie/unitto/feature/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/com/sadellie/unitto/feature/settings/SettingsScreen.kt
@@ -23,17 +23,11 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cached
@@ -44,24 +38,16 @@ import androidx.compose.material.icons.filled.Palette
 import androidx.compose.material.icons.filled.RateReview
 import androidx.compose.material.icons.filled.SwapHoriz
 import androidx.compose.material.icons.filled.Vibration
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.filled._123
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sadellie.unitto.core.base.BuildConfig
@@ -182,35 +168,6 @@ private fun SettingsScreen(
                     icon = Icons.Default.Cached,
                     iconDescription = stringResource(R.string.clear_cache),
                     modifier = Modifier.clickable { clearCache(); showToast(mContext, "👌") },
-                    trailing = {
-                        Box(
-                            Modifier
-                                .clip(CircleShape)
-                                .background(MaterialTheme.colorScheme.errorContainer)
-                                .size(52.dp, 24.dp),
-                            contentAlignment = Alignment.CenterStart
-                        ) {
-                            Box(
-                                Modifier
-                                    .background(
-                                        MaterialTheme.colorScheme.error.copy(alpha = cachePercentage)
-                                            .compositeOver(Color.Green)
-                                    )
-                                    .height(24.dp)
-                                    .fillMaxWidth(cachePercentage),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                if (cachePercentage == 1f) {
-                                    Icon(
-                                        imageVector = Icons.Default.Warning,
-                                        contentDescription = null,
-                                        tint = MaterialTheme.colorScheme.onError,
-                                        modifier = Modifier.size(18.dp)
-                                    )
-                                }
-                            }
-                        }
-                    }
                 )
             }
 
