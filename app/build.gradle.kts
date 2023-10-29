48e75093bd2eb3677a626b9d198b736eee60c693
Sad Ellie <sadellie.dev@gmail.com>
2023-10-29 21:35:11 +0300

    Olive Green

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.sadellie.unitto"
         minSdk = 21
         targetSdk = 34
-        versionCode = 28
+        versionCode = 29
         versionName = "Olive Green"
         resourceConfigurations += setOf(
             "en",
